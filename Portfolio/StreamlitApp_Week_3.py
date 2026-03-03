e34bf7dd3c3b1d3818e7ef8ef29432f121fb05db
kevinjjoynt <kevinjjoynt@gmail.com>
2026-03-03 10:44:34 -0600

    Reorganize imports and maintain functionality

--- Portfolio/StreamlitApp_Week_3.py
+++ Portfolio/StreamlitApp_Week_3.py
@@ -60,7 +60,7 @@ MODEL_INFO = {
         "explainer": 'explainer.shap',
         "pipeline": 'finalized_model.tar.gz',
         "keys": ['AAPL', 'BTC-USD','GOOGL','NVDA', "DEXJPUS", "DEXUSUK", "SP500", "DJIA", "VIXCLS"],
-        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in [['AAPL', 'BTC-USD','GOOGL','NVDA', "DEXJPUS", "DEXUSUK", "SP500", "DJIA", "VIXCLS"]]]
+        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['AAPL', 'BTC-USD','GOOGL','NVDA', "DEXJPUS", "DEXUSUK", "SP500", "DJIA", "VIXCLS"]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -151,6 +151,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
